23efe9c89e2da32d15da8165ef9229a048445a5d
namvdo <van-nam.do@compax.at>
2023-12-08 23:45:19 +0700

    Init slight

---
+++ src/test/java/io/uspeak/slight/SlightApplicationTests.java
@@ -0,0 +1,13 @@
+package io.uspeak.slight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
